3fe477fb4a26485aece16cde4ed382ff05c78864
cesarcamillo <cesaraugustoalvescamillo@hotmail.com>
2021-01-10 20:26:14 -0300

    Adiciona cropping das imagens

--- DataAug.py
+++ DataAug.py
@@ -9,30 +9,47 @@ def load_images(originPath, destPath):
     archives = os.listdir(originPath)
     print ('Doing augmentation')	
     for archive in archives:
-        #Carrega imagem original
-        image = cv2.imread(originPath +'/'+ archive)
+        #Carrega imagem original e inicializa parametros
+        image = cv2.imread(originPath +'/'+ archive)        
 
         #Copia imagem original para a pasta de destino
-        filename = destPath + '/Orig_' + archive
-        cv2.imwrite(filename, image)
+        filename = 'Orig_' + archive
+        cv2.imwrite(destPath +'/'+ filename, image)
+        cropImage(image, destPath, filename)
 
         #Realiza rotacao de 90 graus
-        filename = destPath + '/90Rot_' + archive
+        filename = '90Rot_' + archive
         rotated90 = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-        cv2.imwrite(filename, rotated90)
+        cv2.imwrite(destPath +'/'+ filename, rotated90)
+        cropImage(rotated90, destPath, filename)
 
         #Realiza rotacao de 180 graus
-        filename = destPath + '/180Rot_' + archive
+        filename = '180Rot_' + archive
         rotated180 = cv2.rotate(image, cv2.ROTATE_180)
-        cv2.imwrite(filename, rotated180)
+        cv2.imwrite(destPath +'/'+ filename, rotated180)
+        cropImage(rotated180, destPath, filename)
 
         #Realize rotacao de 270 graus
-        filename = destPath + '/270Rot_' + archive
+        filename = '270Rot_' + archive
         rotated270 = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        cv2.imwrite(filename, rotated270)
+        cv2.imwrite(destPath +'/'+ filename, rotated270)
+        cropImage(rotated270, destPath, filename)
 
     print('Done. Take a look into ' + destPath)
 			
+
+def cropImage (image, destPath, filename):
+    height, width = image.shape[:2]
+    start_row, start_col = int(0), int(0)
+    end_row, end_col = int(height * .5), int(width)
+    cropped_top = image[start_row:end_row , start_col:end_col]
+    cv2.imwrite(destPath +'/Top'+ filename, cropped_top)
+
+    start_row, start_col = int(height * .5), int(0)
+    end_row, end_col = int(height), int(width)
+    cropped_bot = image[start_row:end_row , start_col:end_col]
+    cv2.imwrite(destPath +'/Bottom'+ filename, cropped_bot)
+
 
 if __name__ == "__main__":
 	
